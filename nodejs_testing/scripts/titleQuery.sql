34f52ef551727fb1a5e58d5f8a57210fb43cc1a9
Jon Daniele <jon.daniele@gmail.com>
2015-03-19 12:02:19 -0400

    pushed sql script updates to all dev directories

---
+++ nodejs_testing/scripts/titleQuery.sql
@@ -0,0 +1,14 @@
+/* Get all of the Titles, and therefor the count of the tuples in each table */
+SELECT typeID FROM Types;			#3
+SELECT title FROM Event; 			#19
+SELECT title FROM Personality;			#20
+SELECT title FROM Holding;			#20
+SELECT title FROM Follower;			#20
+SELECT title FROM Attachments;			#20
+SELECT title FROM MonkShugenjaAttachments;	#20
+SELECT title FROM Ring;				#5
+SELECT title FROM Winds;			#9
+SELECT title FROM Stronghold;			#25
+SELECT title FROM Action;			#20
+SELECT title FROM Sensei;			#10
+SELECT title FROM Region;			#20
