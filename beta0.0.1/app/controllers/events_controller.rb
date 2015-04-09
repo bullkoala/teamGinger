cb1228e202b947c8fa53010e317fba5049f7280a
branchsewell <branch.sewell@gmail.com>
2015-04-09 11:59:25 -0400

    searching

--- beta0.0.1/app/controllers/events_controller.rb
+++ beta0.0.1/app/controllers/events_controller.rb
@@ -61,6 +61,15 @@ class EventsController < ApplicationController
     end
   end
 
+  def index
+    if params[:search]
+      @events = Event.search(params[:search]).order('title DESC')
+    else
+      @events = Event.all.order('title DESC')
+    end
+  end
+
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_event
--- beta0.0.1/app/models/event.rb
+++ beta0.0.1/app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ActiveRecord::Base
+
+  def self.search(query)
+  	where("title like ?", "%#{query}")
+  end
   
 end
