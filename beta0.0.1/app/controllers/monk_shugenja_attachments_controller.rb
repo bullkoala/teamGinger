315218d46063b119f18ad50ee1a61cd9d5a4cab3
Jon Daniele <jon.daniele@gmail.com>
2015-04-09 13:07:34 -0400

    fixed ms attachments search

--- beta0.0.1/app/controllers/monk_shugenja_attachments_controller.rb
+++ beta0.0.1/app/controllers/monk_shugenja_attachments_controller.rb
@@ -61,6 +61,15 @@ class MonkShugenjaAttachmentsController < ApplicationController
     end
   end
 
+  #SEARCH monk shugenja attachments
+  def index
+    if params[:search]
+      @monk_shugenja_attachments = MonkShugenjaAttachment.search(params[:search]).order('title DESC')
+    else
+      @monk_shugenja_attachments = MonkShugenjaAttachment.all.order('title DESC')
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_monk_shugenja_attachment
--- beta0.0.1/app/models/monk_shugenja_attachment.rb
+++ beta0.0.1/app/models/monk_shugenja_attachment.rb
@@ -1,3 +1,5 @@
 class MonkShugenjaAttachment < ActiveRecord::Base
-
+  def self.search(query)
+    where("title like ?", "%#{query}")
+  end
 end
