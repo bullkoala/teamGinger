a936b2da4caf433382b0f79bf123a5f41c33b117
Jon Daniele <jon.daniele@gmail.com>
2015-04-11 18:22:42 -0400

    temporarily removed user roles

--- beta0.0.1/app/models/user.rb
+++ beta0.0.1/app/models/user.rb
@@ -5,11 +5,11 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable,
 	 :confirmable, :lockable, :timeoutable
 
-  enum role: [:user, :vip, :admin]
-  after_initialize :set_default_role, :if => :new_record?
+#  enum role: [:user, :vip, :admin]
+#  after_initialize :set_default_role, :if => :new_record?
 
-  def set_default_role
-    self.role ||= :user
-  end
+#  def set_default_role
+#    self.role ||= :user
+#  end
 
 end
