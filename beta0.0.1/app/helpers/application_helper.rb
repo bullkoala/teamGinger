99ab4eca2f40eecaf3652bf1e8a5021ebc0b0731
BullKoala <jon.daniele@gmail.com>
2015-04-19 02:00:18 -0400

    little bit of Bootstrap

--- beta0.0.1/app/helpers/application_helper.rb
+++ beta0.0.1/app/helpers/application_helper.rb
@@ -7,5 +7,14 @@ module ApplicationHelper
 		else
 			"#{page_title} | #{base_title}"
 		end 
-	end 
+	end
+
+#	private
+#	def login_status() 
+#		if user_signed_in?
+#			"#{"You are  logged in as "} #{current_user.email}"
+#		else
+#			"#{"You are not logged into Team Ginger's world"}"
+#		end
+#	end
 end
