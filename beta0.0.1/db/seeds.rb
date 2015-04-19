d3223e611a32cf87007e2eb8a8aa0a3c1da1c1af
Jon Daniele <jon.daniele@gmail.com>
2015-04-19 14:00:08 -0400

    defining new main layout

--- beta0.0.1/db/seeds.rb
+++ beta0.0.1/db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 admin_users = AdminUser.create([{email:'jon@teamginger.com', encrypted_password:'$2a$10$3o5C6CnQrck0jiIUGOzz5uDiZT3Dq47HVUW8NeaeO01bF.OOiFfxC'},{email:'branch@teamginger.com', encrypted_password:'$2a$10$EXJb2TssyYxmm2QS917Orev9BkXZv8MumpuLTMYTomx96fiIRVN6i'},{email:'chuck@teamginger.com', encrypted_password:'$2a$10$.zOivqcxuwEBwrgeXqD.OOAUHMqQeO8u.LSX3kxdIfglmGK9JRmhK'}])
-users = User.create([{email:'ginger@example.com',encrypted_password:'$2a$10$wymcXoX5Wm6tkgcYauKrMenSbG4QEZcZFEADmKn6OUWxTEkze7otK'}])
+users = User.create(email:'ginger@teamginger.com',encrypted_password:'$2a$10$wymcXoX5Wm6tkgcYauKrMenSbG4QEZcZFEADmKn6OUWxTEkze7otK')
