e20a1d3d42976ed58668c29aac515eb1661e090e
BullKoala <jon.daniele@gmail.com>
2015-04-20 22:01:59 -0400

    schema:dump to include new wish table

--- beta0.0.1/db/schema.rb
+++ beta0.0.1/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150416144407) do
+ActiveRecord::Schema.define(version: 20150421010320) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace",     limit: 255
@@ -169,17 +169,22 @@ ActiveRecord::Schema.define(version: 20150416144407) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, null: false
-    t.string   "encrypted_password",     limit: 255, null: false
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
     t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",     limit: 255
     t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "winds", primary_key: "title", force: :cascade do |t|
     t.integer "typeID",      limit: 1,   null: false
@@ -189,5 +194,27 @@ ActiveRecord::Schema.define(version: 20150416144407) do
 
   add_index "winds", ["typeID"], name: "typeID", using: :btree
 
+  create_table "wishes", force: :cascade do |t|
+    t.string  "title",   limit: 255
+    t.string  "url",     limit: 255
+    t.string  "notes",   limit: 255
+    t.integer "user_id", limit: 4
+    t.string  "email",   limit: 255
+  end
+
+  add_index "wishes", ["email"], name: "index_wishes_on_email", using: :btree
+  add_index "wishes", ["user_id"], name: "index_wishes_on_user_id", using: :btree
+
+  add_foreign_key "acts", "types", column: "typeID", primary_key: "typeID", name: "acts_ibfk_1", on_update: :cascade, on_delete: :cascade
+  add_foreign_key "attachments", "types", column: "typeID", primary_key: "typeID", name: "attachments_ibfk_1", on_update: :cascade, on_delete: :cascade
+  add_foreign_key "events", "types", column: "typeID", primary_key: "typeID", name: "events_ibfk_1", on_update: :cascade, on_delete: :cascade
+  add_foreign_key "followers", "types", column: "typeID", primary_key: "typeID", name: "followers_ibfk_1", on_update: :cascade, on_delete: :cascade
+  add_foreign_key "holdings", "types", column: "typeID", primary_key: "typeID", name: "holdings_ibfk_1", on_update: :cascade, on_delete: :cascade
+  add_foreign_key "monk_shugenja_attachments", "types", column: "typeID", primary_key: "typeID", name: "monk_shugenja_attachments_ibfk_1", on_update: :cascade, on_delete: :cascade
+  add_foreign_key "personalities", "types", column: "typeID", primary_key: "typeID", name: "personalities_ibfk_1", on_update: :cascade, on_delete: :cascade
+  add_foreign_key "regions", "types", column: "typeID", primary_key: "typeID", name: "regions_ibfk_1", on_update: :cascade, on_delete: :cascade
+  add_foreign_key "rings", "types", column: "typeID", primary_key: "typeID", name: "rings_ibfk_1", on_update: :cascade, on_delete: :cascade
+  add_foreign_key "senseis", "types", column: "typeID", primary_key: "typeID", name: "senseis_ibfk_1", on_update: :cascade, on_delete: :cascade
+  add_foreign_key "strongholds", "types", column: "typeID", primary_key: "typeID", name: "strongholds_ibfk_1", on_update: :cascade, on_delete: :cascade
   add_foreign_key "winds", "types", column: "typeID", primary_key: "typeID", name: "winds_ibfk_1", on_update: :cascade, on_delete: :cascade
 end
